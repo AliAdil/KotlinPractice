2f14bdbad0ee7515a5afa91e1720ecc03558a817
Ali Adil <aliadilg@gmail.com>
2019-12-30 13:13:03 +0500

    Random Function

--- src/Ams.kt
+++ src/Ams.kt
@@ -1,24 +1,24 @@
 import java.util.*
 
-fun main (args: Array<String>){
-   // dayOfWeek()
-    println("${if(args[0].toInt()<12) "Good morning, Kotlin" else "Good Night, Kotlin"}!")
+fun main(args: Array<String>) {
+    /*dayOfWeek()*/
+    println("${if (args[0].toInt() < 12) "Good morning, Kotlin" else "Good Night, Kotlin"}!")
 
-    val isUnit = println("This is an expression")
+   /* val isUnit = println("This is an expression")
     println(isUnit)
 
     val temperature = 10
     val isHot = temperature > 50
     println(isHot)
 
-    val message = "You are ${if (temperature>50) "fried" else "safe"} fish"
-    println(message)
+    val message = "You are ${if (temperature > 50) "fried" else "safe"} fish"
+    println(message)*/
+    feedTheFish()
 }
 
-fun dayOfWeek (){
-    var dayOfWeek : String ? = null;
+fun dayOfWeek() {
     println("What day is it today")
-    dayOfWeek = when(Calendar.getInstance().get(Calendar.DAY_OF_WEEK)){
+    val dayOfWeek: String? = when (Calendar.getInstance().get(Calendar.DAY_OF_WEEK)) {
         1 -> "Sunday"
         2 -> "Monday"
         3 -> "Tuesday"
@@ -29,4 +29,20 @@ fun dayOfWeek (){
         else -> "Time has Stopped"
     }
     println("Today is $dayOfWeek")
+}
+
+fun feedTheFish(){
+    val day = randomDay()
+    val food = "pellets"
+    println("Today is $day and this fish eat $food")
+
+}
+
+// randomDay() : String {}   String is a returnType
+fun randomDay() : String {
+    val week = listOf("Sunday","Monday","Tuesday","Wednesday","Thrusday","Friday","Saturday","Sunday")
+    //7 is the boundary and excluded
+    //returning week day with random index
+    //Random().nextInt(7) is a builtin function
+    return  week[Random().nextInt(7)]
 }
